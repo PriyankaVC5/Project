3c412bb5da970b521356655ab1cf3eea5d773d27
Chulaki <Priyanka.Chulaki@mind.motherson.com>
2022-06-14 03:04:32 +0530

    initial commit

---
+++ src/test/java/hello/maven/Testcase.java
@@ -0,0 +1,14 @@
+package hello.maven;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Testcase {
+    @Test
+    public void test() {
+        Assert.assertEquals("Hello Maven", new String("Hello Maven"));
+    }
+}
+
+
+	
